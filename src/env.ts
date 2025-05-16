fbfae9fc216a3db28925b28c46ab93a516039b96
kosciolek <45483493+kosciolek@users.noreply.github.com>
2025-05-16 19:59:04 +0200

    stuff

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,3 @@
-export const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY!;
-if (!GOOGLE_PRIVATE_KEY) {
-    throw new Error("GOOGLE_PRIVATE_KEY is not set");
-}
-
 export const SPREADSHEET_ID = process.env.SPREADSHEET_ID!;
 if (!SPREADSHEET_ID) {
     throw new Error("SPREADSHEET_ID is not set");
